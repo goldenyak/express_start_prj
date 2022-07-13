db1bd49d0cb298262a9ca0d67bc339ce67ad4bf8
Egor <goldenyakegor@gmail.com>
2022-07-13 16:16:48 +0300

    init new repo

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req: any, res: any) => {
+    res.send('Hello World!')
+})
+
+app.get('/profile', (req: any, res: any) => {
+    res.send('Hello User!!!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
