93c964386d6344c40ac94f80674b887172ad74e3
Egor <goldenyakegor@gmail.com>
2022-07-14 20:24:29 +0300

    create basic videos api

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,65 @@
 import express, {Request, Response} from "express";
-const app = express()
-const port = process.env.PORT || 3001
+import cors from 'cors';
+import bodyParser from "body-parser";
 
+const app = express();
+app.use(cors());
+app.use(bodyParser());
+const port = process.env.PORT || 5000
+
+const videos = [
+    {id: 1, title: 'About JS - 01', author: 'it-incubator.eu'},
+    {id: 2, title: 'About JS - 02', author: 'it-incubator.eu'},
+    {id: 3, title: 'About JS - 03', author: 'it-incubator.eu'},
+    {id: 4, title: 'About JS - 04', author: 'it-incubator.eu'},
+    {id: 5, title: 'About JS - 05', author: 'it-incubator.eu'},
+]
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!')
+    res.send("Салют, мэн)))")
+})
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videos)
+})
+app.get('/videos/:videoId', (req: Request, res: Response) => {
+    // const id = +req.params.videoId;
+    let videoById = videos.find(el => el.id === +req.params.videoId)
+    if (videoById) {
+        res.send(`${videoById.title}`)
+    } else {
+        res.send(404)
+    }
+})
+app.post('/videos', (req: Request, res: Response) => {
+    const newVideo = {
+        id: +(new Date()),
+        title: req.body.title,
+        author: "it-incubator.eu"
+    }
+    videos.push(newVideo)
+    res.status(201).send(videos)
+})
+app.put('/videos/:id',(req: Request, res: Response)=>{
+    for ( let i = 0; i < videos.length; i++) {
+        if (videos[i].id === +req.params.id) {
+            videos[i].title = req.body.title
+            res.status(201).send(videos)
+            return;
+        }
+    }
+    res.send(404)
+})
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < videos.length; i++) {
+        if (videos[i].id === +req.params.id) {
+            videos.splice(i, 1)
+            res.send(204)
+            return;
+        }
+    }
+    res.send(404)
 })
 
-app.get('/user', (req: Request, res: Response) => {
-    res.send('Hello User!')
-})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
