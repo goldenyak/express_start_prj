1a1daf0bdca5bc5b621695d9f266254c1e08774d
Egor <goldenyakegor@gmail.com>
2022-07-20 23:24:47 +0300

    refactoring code

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -4,12 +4,18 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const authorized: boolean = req.headers.authorization?.split(" ")[1].toString() === 'YWRtaW46cXdlcnR5'
     const authType: string = req.headers.authorization?.split(" ")[0].toString() || 'no auth'
 
-    if (req.headers.authorization && authType==='Basic' && authorized === true) {
+    if (req.headers.authorization && authType === 'Basic' && authorized === true) {
         next()
-    }
-    else {
+    } else {
         res.status(401)
         res.end()
         return
+    }
+    if (req.headers["content-type"] === "application/json") {
+        next()
+    } else {
+        res.status(400).send("Bad content type")
+        res.end()
+        return;
     }
 }
