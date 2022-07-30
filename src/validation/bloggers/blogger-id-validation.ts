0a3a924f76dd9b54d942da0d56d59506700be261
Egor <goldenyakegor@gmail.com>
2022-07-30 18:00:41 +0300

    refactor code

--- src/validation/bloggers/blogger-id-validation.ts
+++ src/validation/bloggers/blogger-id-validation.ts
@@ -1,12 +1,19 @@
 import {NextFunction, Request, Response} from "express";
 import {bloggersCollection} from "../../db/db";
+import {bloggersRepository} from "../../repositories/bloggers-repository";
 
-export const bloggerIdValidation = (req: Request, res: Response, next: NextFunction) => {
-    const bloggerId = +req.params.id
-    if (!bloggersCollection.findOne({id: bloggerId})) {
-        res.status(404)
+export const bloggerIdValidation =  async (req: Request, res: Response, next: NextFunction) => {
+    // const bloggerId = +req.params.id
+    // if (!bloggersCollection.findOne({id: bloggerId})) {
+    //     res.status(404)
+    //     return
+    // } else {
+    //     next()
+    // }
+
+    const bloggerId = +req.params.bloggerId || +req.params.id ||  null
+    if(bloggerId && !await bloggersRepository.getBloggerById(bloggerId)) {
+        res.sendStatus(404)
         return
-    } else {
-        next()
-    }
+    } else next()
 };
