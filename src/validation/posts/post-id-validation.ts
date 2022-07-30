b54a8ee58f1b7e7a17bd265faeef2b7705adfa2d
Egor <goldenyakegor@gmail.com>
2022-07-30 18:38:06 +0300

    refactor code

--- src/validation/posts/post-id-validation.ts
+++ src/validation/posts/post-id-validation.ts
@@ -5,7 +5,15 @@ import {bloggersRepository} from "../../repositories/bloggers-repository";
 import {postsRepository} from "../../repositories/posts-repository";
 
 
-export const postIdValidation = async (req: Request, res: Response, next: NextFunction) => {
+export const postIdValidation = (req: Request, res: Response, next: NextFunction) => {
+
+    const postId = +req.params.postId || +req.params.id || null
+    const exist = postId ? postsServices.getPostById(postId) : null
+    if (!exist) {
+        res.status(404)
+        return
+    } else next()
+
     // const postId = +req.params.id
     //
     // if (!postsCollection.findOne({id: postId})) {
@@ -15,9 +23,9 @@ export const postIdValidation = async (req: Request, res: Response, next: NextFu
     //     next()
     // }
 
-    const postId = +req.params.bloggerId || +req.params.id ||  null
-    if(postId && !await postsRepository.getPostById(postId)) {
-        res.sendStatus(404)
-        return
-    } else next()
+    // const postId = +req.params.bloggerId || +req.params.id ||  null
+    // if(postId && !await postsRepository.getPostById(postId)) {
+    //     res.sendStatus(404)
+    //     return
+    // } else next()
 };
