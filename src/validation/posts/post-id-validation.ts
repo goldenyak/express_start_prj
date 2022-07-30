c465f7f96702974c646d737a7874e5110e7ee29c
Egor <goldenyakegor@gmail.com>
2022-07-30 18:22:46 +0300

    refactor code

--- src/validation/posts/post-id-validation.ts
+++ src/validation/posts/post-id-validation.ts
@@ -1,15 +1,23 @@
 import {NextFunction, Request, Response} from "express";
 import {postsCollection} from "../../db/db";
 import {postsServices} from "../../services/posts-services";
+import {bloggersRepository} from "../../repositories/bloggers-repository";
+import {postsRepository} from "../../repositories/posts-repository";
 
 
-export const postIdValidation = (req: Request, res: Response, next: NextFunction) => {
-    const postId = +req.params.id
+export const postIdValidation = async (req: Request, res: Response, next: NextFunction) => {
+    // const postId = +req.params.id
+    //
+    // if (!postsCollection.findOne({id: postId})) {
+    //     res.status(404)
+    //     return
+    // } else {
+    //     next()
+    // }
 
-    if (!postsCollection.findOne({id: postId})) {
-        res.status(404)
+    const postId = +req.params.bloggerId || +req.params.id ||  null
+    if(postId && !await postsRepository.getPostById(postId)) {
+        res.sendStatus(404)
         return
-    } else {
-        next()
-    }
+    } else next()
 };
